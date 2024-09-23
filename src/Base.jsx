1daaaf798881ddbcb3ce5e48e8905468ec11db0e
rootnure <nur.diu.2791@gmail.com>
2024-09-24 00:17:10 +0600

    Fake data added and loaded dynamically in component

---
+++ src/Base.jsx
@@ -0,0 +1,11 @@
+import Blogs from "./components/Blogs/Blogs";
+import Header from "./components/Header/Header";
+
+export default function Base() {
+    return (
+        <>
+            <Header></Header>
+            <Blogs></Blogs>
+        </>
+    )
+}
